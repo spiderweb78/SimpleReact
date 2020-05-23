32c412f59d8349a71715af097bf63b5ca4ac1643
spiderweb78 <hlaingminoo7777@gmail.com>
2020-05-23 23:11:29 +0630

    Updates

---
+++ precache-manifest.dc19fb4d13173752bf7d6998ed11888c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e90dd309559cd52b74cff9d305fe8a82",
+    "url": "/SimpleReact/index.html"
+  },
+  {
+    "revision": "1c7be5df0444e0f90c87",
+    "url": "/SimpleReact/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f1e4a1c66f85993e14da",
+    "url": "/SimpleReact/static/js/2.dce16a95.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/SimpleReact/static/js/2.dce16a95.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1c7be5df0444e0f90c87",
+    "url": "/SimpleReact/static/js/main.634c0863.chunk.js"
+  },
+  {
+    "revision": "2ce413dc5dcff7a8bbd0",
+    "url": "/SimpleReact/static/js/runtime-main.6054447b.js"
+  }
+]);
